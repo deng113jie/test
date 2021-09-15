c7610523da7c6381ab576c5850fb7083dce7f8f5
unknown <deng113jie@gmail.com>
2021-09-15 10:51:55 +0100

    os dep

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 from Cython.Build import cythonize
 import os
 
-setup(
-    if os.name == 'nt':
-        srcpath= 'src\\helloworld.pyx'
-    else:
-        srcpath= 'src/helloworld.pyx'
-    
+if os.name == 'nt':
+    srcpath= 'src\\helloworld.pyx'
+else:
+    srcpath= 'src/helloworld.pyx'
+
+setup(    
     name="cytest",
     version="0.0.1",
     ext_modules = cythonize(srcpath)
